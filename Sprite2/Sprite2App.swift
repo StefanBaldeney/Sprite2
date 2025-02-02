6189553eecd2999a6c507fae1b52cbd578073d66
Stefan Brandt <stefan-brandt@live.de>
2025-02-02 09:31:01 +0100

    Initial Commit

---
+++ Sprite2/Sprite2App.swift
@@ -0,0 +1,17 @@
+//
+//  Sprite2App.swift
+//  Sprite2
+//
+//  Created by Stefan Brandt on 02.02.25.
+//
+
+import SwiftUI
+
+@main
+struct Sprite2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
